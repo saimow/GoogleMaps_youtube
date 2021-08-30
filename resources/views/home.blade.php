bf76ccda47807deb536946b995989d55b253b1bb
Mohamed Hafidi <MohamedH-a@hotmail.com>
2021-08-30 21:41:50 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,7 +48,7 @@
                         })
                     }
                 </script>
-                <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTIRjFnzd4c0W3Ll6TTi_O2O1VTUfnu5o&callback=initMap"
+                <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
                         type="text/javascript"></script>
             </div>
 
